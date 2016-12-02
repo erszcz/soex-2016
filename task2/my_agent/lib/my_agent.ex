60a6d59f8375fa5523e74c7b0dc320d1504c240b
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2016-12-02 15:36:08 +0100

    Implement MyAgent

--- task2/my_agent/lib/my_agent.ex
+++ task2/my_agent/lib/my_agent.ex
@@ -1,6 +1,22 @@
 defmodule MyAgent do
   use GenServer
 
-  def start(_, _), do: raise "not implemented yet"
+  def start(fun, options \\ []), do:
+    GenServer.start(__MODULE__, fun, options)
+
+  def stop(name), do: GenServer.stop(name)
+
+  def init(fun), do: {:ok, fun.()}
+
+  def get(name, fun), do: GenServer.call(name, {:get, fun})
+
+  def update(name, fun), do: GenServer.call(name, {:update, fun})
+
+  def handle_call({:get, fun}, _from, state), do:
+    {:reply, fun.(state), state}
+  def handle_call({:update, fun}, _from, state), do:
+    {:reply, :ok, fun.(state)}
+  def handle_call(request, from, state), do:
+    super(request, from, state)
 
 end
